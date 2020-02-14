23192a88e1138072d27dd61991f9cb62a497eeca
ed <anax@hotmail.co.uk>
2020-02-14 14:23:42 +0000

    formatting updates to bastion-vm scripts

--- bastion-tunnel/delete-bastion-vm.sh
+++ bastion-tunnel/delete-bastion-vm.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -u
 
+# stop the bastion machine
+gcloud compute instances stop \
+  ${BASTION_NAME} \
+  --project ${GOOGLE_CLOUD_PROJECT} \
+  --zone ${ZONE} \
+  --quiet
+
 # delete the bastion machine
-gcloud compute \
-  instances delete \
+gcloud compute instances delete \
   ${BASTION_NAME} \
   --project ${GOOGLE_CLOUD_PROJECT} \
   --zone ${ZONE} \
