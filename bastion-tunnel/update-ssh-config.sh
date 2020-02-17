98cb1e46e5c01f9ba0d01a7bfd4f456315474cc5
ed <anax@hotmail.co.uk>
2020-02-17 13:35:51 +0000

    updated vm startup script to be self-deleting vm

--- bastion-tunnel/update-ssh-config.sh
+++ bastion-tunnel/update-ssh-config.sh
@@ -1,4 +1,12 @@
 #! /bin/bash -u
 
+# allow root ssh access
 sudo sed -i -e 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo service sshd restart
+
+# self deleting vm
+# https://cloud.google.com/community/tutorials/create-a-self-deleting-virtual-machine
+sleep 3600s
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+gcloud --quiet compute instances delete $NAME --zone=$ZONE
